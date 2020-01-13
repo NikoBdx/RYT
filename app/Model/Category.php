4c0518c3681909c647a0e6fd6449453beed2e6b8
Mikl3124 <mickael.delpech@gmail.com>
2020-01-13 16:40:51 +0100

    Add architecture shema db

---
+++ app/Model/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Model\Category;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model 
+{
+
+    protected $table = 'categories';
+    public $timestamps = true;
+
+}
