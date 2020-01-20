6674b0a0aea12c8733b11721c82f8eee6a28627c
Mikl3124 <mickael.delpech@gmail.com>
2020-01-20 09:11:25 +0100

    add user seed

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -22,23 +22,9 @@ class UsersSeeder extends Seeder
             'town'  => 'Bordeaux',
             'cp'    => '33000',
             'vehicule'  => 'null',
-            'latitude'  => 'null',
-            'longitude' => 'null',
-        ]);
-
-        DB::table('users')->insert([
-            'firstname' => 'User',
-            'lastname'  => 'website',
-            'email' => 'user@gmail.com',
-            'password' => bcrypt('1234'),
-            'email_verified_at' => '2020-01-01 00:00:00',
-            'role'  => 'admin',
-            'address' => '18-rue Abbé de l\'epée',
-            'town'  => 'Bordeaux',
-            'cp'    => '33800',
-            'vehicule'  => 'null',
-            'latitude'  => 'null',
-            'longitude' => 'null',
+            'latitude'  => '45.0582',
+            'longitude' => '0.418498',
+            'email_verified_at' => '2020-01-17 10:21:11'
         ]);
 
     }
