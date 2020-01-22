205f8e9921f57b3bc1ffda39c328bff5292bdf9f
Mikl3124 <mickael.delpech@gmail.com>
2020-01-22 14:38:27 +0100

    Bug ok

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -22,10 +22,9 @@ class DriverController extends Controller
   public function index()
   {
 
-    $orders = Order::all();
-    $orders_start = $orders->where('status', '=', 'start')->latest()->get();
-    $orders_pending = $orders->where('status', '=', 'pending')->latest()->get();
-    $orders_done = $orders->where('status', '=', 'done')->latest()->get();
+    $orders_start = Order::where('status', 'start')->latest()->get();
+    $orders_pending = Order::where('status', 'pending')->latest()->get();
+    $orders_done = Order::where('status', 'done')->latest()->get();
     return view('drivers.courses', compact('orders_start', 'orders_pending', 'orders_done'));
   }
 
